4d7079c0dccff83cae891755a38373065929af9c
Elliott Jackson <elliott.w.jackson@gmail.com>
2021-12-17 12:47:16 -0700

    add cleansed myconfig-copy.py back

---
+++ myconfig-copy.py
@@ -0,0 +1,34 @@
+
+# configure location of the everquest log files
+BASE_DIRECTORY              = 'c:\\users\\ewjax\\Documents\\Gaming\\Everquest-Project1999'
+DEFAULT_CHAR_NAME           = 'Azleep'
+
+LOGS_DIRECTORY              = '\\logs\\'
+SERVER_NAME                 = 'P1999Green'
+
+# used in hearbeat test.  Max number of seconds of inactivity before it checks to see if a new file is being written to
+HEARTBEAT                   = 15
+
+# default randomevent window
+DEFAULT_WINDOW              = 30
+
+# timeouit for spell casting pending wait timeout
+SPELL_PENDING_TIMEOUT_SEC   = 10
+
+# timeout for combat timemr
+COMBAT_TIMEOUT_SEC          = 120
+
+# token corresponds to the bot from discord developer portal
+BOT_TOKEN                   = 'your bot token here'
+
+# change this if you need to have multiple bots with same commands present on a server
+BOT_COMMAND_PREFIX          = '.'
+
+
+# some server ID's
+PERSONAL_SERVER_NAME        = 'ewjax personal server'
+PERSONAL_SERVER_POPID       = pop_channel_id_here
+PERSONAL_SERVER_ALERTID     = alert_channel_id_here
+PERSONAL_SERVER_VALETID     = eqvalet_channel_id_here
+
+
